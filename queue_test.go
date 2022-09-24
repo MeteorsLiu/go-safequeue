e5c1c3ab72984e0eaf81d78e65e61a285e6ab7e4
GuoHaoLan <coopticks@hotmail.com>
2022-09-24 14:42:20 +0800

    A

--- queue_test.go
+++ queue_test.go
@@ -18,3 +18,20 @@ func TestQueue(t *testing.T) {
 		}
 	}
 }
+
+func TestQueueParallel(t *testing.T) {
+	tq := []int{1, 3, 5, 7, 9}
+	q := New[int]()
+	for _, v := range tq {
+		go q.Push(v)
+		t.Logf("Push %d", v)
+	}
+	for {
+		v, ok := q.Pop()
+		t.Log(v, ok)
+		if !ok {
+			t.Log("return")
+			return
+		}
+	}
+}
